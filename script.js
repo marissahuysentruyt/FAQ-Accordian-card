e0f320200fb12f581cc5aa497ddcb49c910dd215
marissahuysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>
2021-12-02 20:11:31 -0500

    FAQ card
    -semantic updates on index.html/styles
    -adjusted script file to display .answer when clicked

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-// display answers
+// Display FAQ Answers on click
+
+// question variable, should return nodelist
 let questions = document.querySelectorAll(".visible");
-let answer = document.getElementsByClassName("answer");
-let animation = document.querySelectorAll(".arrow");
 
+// loop through questions
+for(let question of questions) {
+    question.addEventListener("click", function(event) {
+        //new variable, storing the event target 
+        const clicked = event.target;
+        //new variable, create displayed answer ('js-dropdown' & 'active')
+        const displayed = document.querySelector(".js-dropdown");
 
-function showAnswer() {
-    for(i = 0; i <=4; i++) {
-        if(answer.style.display = "none") {
-            answer.style.display = "block";
-        } else {
-            answer.style.display = "none";
-        }   
-    } 
-}
-
-function animate() {
-    animation.style.animation = "pointUp 0.4s forwards 1";
-}
-
-questions.offclick = function() {
-    answer.style.display = "none";
+        // if the clicked item has a class of js-dropdown, remove it
+        if(displayed) {
+            displayed.classList.remove(".js-dropdown");
+        } 
+        //otherwise, add js-dropdown to the clicked item
+        clicked.classList.add(".js-dropdown");
+       
+        //answer variable, should return a nodelist
+        let answers = document.querySelectorAll(".invisible");
+        
+        // loop through answers 
+        for(let answer of answers) {
+            //if the index-numbers (which were hardcoded) match, display and change fontWeight
+            if(answer.getAttribute("index-number") === question.getAttribute("index-number")) {
+                answer.style.display = "block";
+                question.style.fontWeight = "700";
+            } else {
+                answer.style.display = "none";
+                // question.style.fontWeight = "400"; //this is overriding the if statement, except for last question
+                
+            }
+        }
+        
+    });
+    
 }
