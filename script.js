60854c12090ec2fc0816b72a2014c7b1bde551cd
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2023-02-28 16:08:42 -0500

    :sparkles: feat: match design and functionality
    Update script to simplify accordion behavior.
    Update styles to represent new HTML naming/structure

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-// Display FAQ Answers on click
+const getAccordionButtons = () => {
+    const accordion = document.querySelector(".faq-accordion");
+    return accordion.querySelectorAll("button");
+};
 
-// question variable, should return nodelist
-let questions = document.querySelectorAll(".visible");
+const findCollapsableHeaders = () => {
+    const accordion = document.querySelector(".faq-accordion");
+    return accordion.querySelectorAll("h3");
+};
 
-// loop through questions
-for(let question of questions) {
-    question.addEventListener("click", function(event) {
-        //new variable, storing the event target 
-        const clicked = event.target;
-        //new variable to create displayed answer, giving class to the question ('js-dropdown')
-        const displayed = document.querySelector(".js-dropdown");
-
-        // if the clicked item has a class of js-dropdown, remove it
-        if(displayed) {
-            displayed.classList.remove(".js-dropdown");
-        } 
-        //otherwise, add js-dropdown to the clicked item
-        clicked.classList.add(".js-dropdown");
-       
-        //answer variable, should return a nodelist
-        let answers = document.querySelectorAll(".invisible");
-        
-        // loop through answers 
-        for(let answer of answers) {
-            //if the index-numbers (which were hardcoded) match, display and change fontWeight
-            if(answer.getAttribute("index-number") === question.getAttribute("index-number")) {
-                answer.style.display = "block";
-                question.style.fontWeight = "700";
-            // if they don't match, don't display
-            } else {
-                answer.style.display = "none";
-                // question.style.fontWeight = "400"; //this is overriding the if statement, except for last question
-                
-            }
-
-            // carrot variables- NOT FIGURED OUT! onclick="animation()" is already in the html file. if the carrot & answer index-number are the same, AND the answer is displayed, I want to trigger the carrot to point upward, all with the question click. .animate is in the stylesheet...not sure. 
-            let carrot = document.querySelectorAll(".carrot");
-            // animation function for carrot? 
-            animation = () => {
-            if(carrot.getAttribute("index-number") === answer.getAttribute("index-number") && (answer.style.display = "block")) {
-                carrot.classList.add(".animate");
-            }
-        }
-        }
-        
+const collapseAll = () => {
+    findCollapsableHeaders().forEach((header) => {
+        header.classList.add("collapsed");
     });
-    
-}
+    getAccordionButtons().forEach((button) => {
+        button.setAttribute("aria-expanded", "false");
+    })
+};
+
+const handleAccordionClick = (e) => {
+    const panelWasClosed = e.target.getAttribute("aria-expanded") === "false";
+
+    collapseAll();
+
+    if(panelWasClosed) {
+        e.target.setAttribute("aria-expanded", true);
+        e.target.parentElement.classList.remove("collapsed");
+    }
+};
+
+const addButtonListeners = () => {
+    getAccordionButtons().forEach((button) => {
+        button.addEventListener("click", handleAccordionClick);
+    });
+};
+
+addButtonListeners();
